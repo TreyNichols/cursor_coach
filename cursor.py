3a0c54328fb8deddc9b207970106055d566f16fd
Laurel Pinckley <laurelpinckley@gmail.com>
2024-10-26 16:16:03 -0500

    Fixed the rect of the cursor so it can act as a rect and tweaked the cursor image

--- cursor.py
+++ cursor.py
@@ -1,9 +1,7 @@
 # Imports
 import pygame
 import mouse
-from constants import *
-
-CURSOR_SIZE = 32
+from constants import (SCREEN_WIDTH, SCREEN_HEIGHT, CURSOR_SIZE)
 
 class My_Cursor():
     def __init__(self, sensitivity:float):
@@ -12,7 +10,7 @@ class My_Cursor():
         self.root = pygame.display.get_surface()
         self.x = SCREEN_WIDTH/2
         self.y = SCREEN_HEIGHT/2
-        self.rect = pygame.rect.Rect(self.x, self.y, 32, 32)
+        self.rect = pygame.rect.Rect(self.x + CURSOR_SIZE//2, self.y + CURSOR_SIZE//2, 2, 2)
         self.sensitivity = sensitivity
         
         # Makes the hardware mouse invisible
@@ -22,15 +20,17 @@ class My_Cursor():
 
     def draw(self):
         self.root.blit(self.spr, (self.x, self.y))
-        s = pygame.Surface((CURSOR_SIZE, CURSOR_SIZE))
+        s = pygame.Surface((2, 2))
         self.root.blit(s, (self.rect.x, self.rect.y))
         
     def update(self):
         # Finds the change in position of the mouse from where it was prior to now
         # and adds that to the cursor position times the sensitivity factor
         (delta_x, delta_y) = pygame.mouse.get_rel()
-        self.x += (delta_x * sensitivity)
-        self.y += (delta_y * sensitivity)
+        self.x += (delta_x * self.sensitivity)
+        self.y += (delta_y * self.sensitivity)
+        self.rect.x += (delta_x * self.sensitivity)
+        self.rect.y += (delta_y * self.sensitivity)
         
         # Keeps the cursor on the screen
         if self.x < 0 - (CURSOR_SIZE // 2):
@@ -41,6 +41,15 @@ class My_Cursor():
             self.y = 0 - (CURSOR_SIZE // 2)
         if self.y >= SCREEN_HEIGHT - (CURSOR_SIZE // 2):
             self.y = SCREEN_HEIGHT - (CURSOR_SIZE // 2)
+            
+        if self.rect.x < -1:
+            self.rect.x = -1
+        if self.rect.x > SCREEN_WIDTH - 1:
+            self.rect.x = SCREEN_WIDTH - 1
+        if self.rect.y < -1:
+            self.rect.y = -1
+        if self.rect.y >= SCREEN_HEIGHT - 1:
+            self.rect.y = SCREEN_HEIGHT - 1
         
         # Moves mouse if it hits the edge
         if (pygame.mouse.get_pos()[0] == 0 or pygame.mouse.get_pos()[0] >= SCREEN_WIDTH-1 or 
@@ -61,29 +70,31 @@ class My_Cursor():
 # Take amount moved each frame and translate it to the artificial cursor times sensitivity factor x
 
 # Testing:
-
-# pygame.event.set_grab(True)
-# c = My_Cursor()
-# run = True
-# pygame.mouse.set_pos((SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
-# c.update()
-# c.draw()
-# pygame.display.update()
-# while run:
-#     SCREEN.fill("white")
-#     for event in pygame.event.get():
-#         if event.type == KEYDOWN:
-#             # Was it the Escape key? If so, stop the loop.
-#             if event.key == K_ESCAPE:
-#                 running = False
-#         elif event.type == pygame.QUIT:
-#                 pygame.quit()
-#                 sys.exit()
-#         if event.type == pygame.MOUSEMOTION:
-#             c.update()
-#     c.draw()
-#     pygame.display.update()
-# #c = pygame.cursors.Cursor()
+from pygame.locals import KEYDOWN
+pygame.init()
+pygame.event.set_grab(True)
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+c = My_Cursor(1)
+run = True
+pygame.mouse.set_pos((SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+c.update()
+c.draw()
+pygame.display.update()
+while run:
+    SCREEN.fill("white")
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            # Was it the Escape key? If so, stop the loop.
+            if event.key == K_ESCAPE:
+                running = False
+        elif event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        if event.type == pygame.MOUSEMOTION:
+            c.update()
+    c.draw()
+    pygame.display.update()
+#c = pygame.cursors.Cursor()
 
 
 
